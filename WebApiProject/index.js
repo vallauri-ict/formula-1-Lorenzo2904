b51de870ee82add08cc66d98d2838675e5b15c70
Lorenzo2904 <l.mori.0279@vallauri.edu>
2020-05-13 19:04:48 +0200

    Commit

--- WebApiProject/index.js
+++ WebApiProject/index.js
@@ -1,9 +1,10 @@
 ﻿"use strict";
 
 $(() => {
+    $("#info").hide();
     $("#caricaDrivers").on("click", () => {
         $("#wrapper").empty();
-        $("#info").empty();
+        $("#info").hide();
         richiesta("/drivers/list", data => {
             creaTabellaDrivers(data);
         });
@@ -11,7 +12,7 @@ $(() => {
 
     $("#caricaTeams").on("click", () => {
         $("#wrapper").empty();
-        $("#info").empty();
+        $("#info").hide();
         richiesta("/teams/list", data => {
             creaTabellaTeams(data);
         });
@@ -19,7 +20,7 @@ $(() => {
 
     $("#caricaCircuits").on("click", () => {
         $("#wrapper").empty();
-        $("#info").empty();
+        $("#info").hide();
         richiesta("/circuits/list", data => {
             creaTabellaCircuits(data);
         });
@@ -28,9 +29,23 @@ $(() => {
 
 function creaTabellaDrivers(drivers)
 {
+    let color = "a";
     let _table = $("<table>");
+    _table.prop("id", "tableData");
+    let _thead = $("<thead>");
+    _thead.append("<th>ID</th><th>Name</th><th>Surname</th><th>Country</th><th>Image</th><th>Infos</th>")
+    _table.append(_thead);
+    let _tbody = $("<tbody>");
     for (let i = 0; i < drivers.length; i++) {
         let row = $("<tr>");
+        if (color == "a") {
+            row.prop("style", "background-color: #fff8dc;");
+            color = "b";
+        }
+        else {
+            row.prop("style", "background-color: #fffaf0;");
+            color = "a";
+        }
         for (let index in drivers[i]) {
             let cell = $("<td>");
             if (index != "img") {
@@ -41,19 +56,25 @@ function creaTabellaDrivers(drivers)
                 let _img = $("<img>");
                 _img.prop("src", drivers[i][index]);
                 _img.prop("height", "50");
-                row.append(_img);
+                cell.append(_img);
+                row.append(cell);
             }
         }
-        let btnInfo = $(`<input type='button' onclick='visualizzaDriver(${drivers[i]["id"]})' value='Info'>`);
-        row.append(btnInfo);
-        _table.append(row);
+        let btnInfo = $(`<a href='#bannerMenu'><input type='button' onclick='visualizzaDriver(${drivers[i]["id"]})' value='Click here'></a>`);
+        btnInfo.prop("style", "width: 150px; heigth: 30px;");
+        let cell = $("<td>");
+        cell.append(btnInfo);
+        row.append(cell);
+        _tbody.append(row);
     }
+    _table.append(_tbody);
     $("#wrapper").append(_table);
 }
 
 function visualizzaDriver(id) {
     let info = $("#info");
     info.empty();
+    info.show();
     richiesta(`/drivers/${id}`, data => {
         console.log(data);
         let nome = data["firstname"];
@@ -63,26 +84,41 @@ function visualizzaDriver(id) {
         let pob = data["placeOfBirthday"];
         let desc = data["description"];
         let naz = data["country"]["countryName"];
-        info.append($(`<h1>${nome} ${cognome}</h1>`));
-        let p = $(`<p>Date of Birth: ${dob.split("T")[0]}</p>`);
-        info.append(p);
-        p = $(`<p>Place of Birth: ${pob}</p>`);
-        info.append(p);
-        p = $(`<p>Nationality: ${naz}</p>`);
-        info.append(p);
+        info.append($(`<h1 style="text-align: center;">${nome} ${cognome}</h1>`));
         let img = $("<img>");
         img.prop("src", foto);
-        img.prop("height", "150");;
-        info.append(img);
-        p = $(`<p>Description: ${desc}</p>`);
+        img.prop("height", "250");
+        let div = $("<div>");
+        div.prop("style", "text-align: center; margin-bottom: 10px;");
+        div.append(img);
+        info.append(div);
+        let p = $(`<p style='text-align: center; margin-bottom: 5px;'><b>Date of Birth</b>: ${dob.split("T")[0]}, ${pob}</p>`);
+        info.append(p);
+        p = $(`<p style='text-align: center;'><b>Nazionality</b>: ${naz}</p>`);
+        info.append(p);
+        p = $(`<p><b>Description</b>: ${desc}</p>`);
         info.append(p);
     });
 }
 
 function creaTabellaTeams(teams) {
+    let color = "a";
     let _table = $("<table>");
+    _table.prop("id", "tableData");
+    let _thead = $("<thead>");
+    _thead.append("<th>ID</th><th>Brand</th><th>Country</th><th>Logo</th><th>Name</th><th>Surname</th><th>Image</th><th>Infos</th>")
+    _table.append(_thead);
+    let _tbody = $("<tbody>");
     for (let i = 0; i < teams.length; i++) {
         let row = $("<tr>");
+        if (color == "a") {
+            row.prop("style", "background-color: #fff8dc;");
+            color = "b";
+        }
+        else {
+            row.prop("style", "background-color: #fffaf0;");
+            color = "a";
+        }
         for (let index in teams[i]) {
             let cell = $("<td>");
             if (index != "img" && index != "logo") {
@@ -93,19 +129,25 @@ function creaTabellaTeams(teams) {
                 let _img = $("<img>");
                 _img.prop("src", teams[i][index]);
                 _img.prop("height", "50");
-                row.append(_img);
+                cell.append(_img);
+                row.append(cell);
             }
         }
-        let btnInfo = $(`<input type='button' onclick='visualizzaTeam(${teams[i]["id"]})' value='Info'>`);
-        row.append(btnInfo);
-        _table.append(row);
+        let btnInfo = $(`<a href='#bannerMenu'><input type='button' onclick='visualizzaTeam(${teams[i]["id"]})' value='Click Here'></a>`);
+        btnInfo.prop("style", "width: 150px; heigth: 30px;");
+        let cell = $("<td>");
+        cell.append(btnInfo);
+        row.append(cell);
+        _tbody.append(row);
     }
+    _table.append(_tbody);
     $("#wrapper").append(_table);
 }
 
 function visualizzaTeam(id) {
     let info = $("#info");
     info.empty();
+    info.show();
     richiesta(`/teams/${id}`, data => {
         console.log(data);
         let chassis = data["chassis"];
@@ -119,28 +161,46 @@ function visualizzaTeam(id) {
         let techChief = data["technicalChief"];
         let secondDriver = data["secondDriver"]["firstname"] + " " + data["secondDriver"]["lastname"];
 
-        info.append($(`<h1>${nome}</h1>`));
+        info.append($(`<h1 style='text-align: center;'>${nome}</h1>`));
         let img = $("<img>");
         img.prop("src", logo);
         img.prop("height", "150");
-        info.append(img);
-        info.append($(`<p>Full Team Name: ${nomeCompleto}</p>`));
-        info.append($(`<p>Country: ${paese}</p>`));
-        info.append($(`<p>Chassis: ${chassis}</p>`));
-        info.append($(`<p>Power Unit: ${pU}</p>`));
-        info.append($(`<p>Technical Chief: ${techChief}</p>`));
-        info.append($(`<p>Drivers: ${primoDriver}, ${secondDriver}</p>`));
+        let div = $("<div style='text-align: center; margin-bottom: 20px;'>");
+        div.append(img);
+        info.append(div);
+        info.append($(`<p style='text-align: center;'><b>Full Team Name</b>: ${nomeCompleto}</p>`));
+        info.append($(`<p style='text-align: center;'><b>Country</b>: ${paese}</p>`));
+        info.append($(`<p style='text-align: center;'><b>Chassis</b>: ${chassis}</p>`));
+        info.append($(`<p style='text-align: center;'><b>Power Unit</b>: ${pU}</p>`));
+        info.append($(`<p style='text-align: center;'><b>Technical Chief</b>: ${techChief}</p>`));
+        info.append($(`<p style='text-align: center;'><b>Drivers</b>: ${primoDriver}, ${secondDriver}</p>`));
         img = $("<img>");
         img.prop("src", foto);
         img.prop("height", "150");
-        info.append(img);
+        div = $("<div style='text-align: center;'>");
+        div.append(img);
+        info.append(div);
     });
 }
 
 function creaTabellaCircuits(circuits) {
+    let color = "a";
     let _table = $("<table>");
+    _table.prop("id", "tableCircuits");
+    let _thead = $("<thead>");
+    _thead.append("<th>ID</th><th>Circuit Name</th><th>Lenght (m)</th><th>Laps</th><th>Country</th><th>Image</th>")
+    _table.append(_thead);
+    let _tbody = $("<tbody>");
     for (let i = 0; i < circuits.length; i++) {
         let row = $("<tr>");
+        if (color == "a") {
+            row.prop("style", "background-color: #fff8dc;");
+            color = "b";
+        }
+        else {
+            row.prop("style", "background-color: #fffaf0;");
+            color = "a";
+        }
         for (let index in circuits[i]) {
             if (index == "recordLap")
                 continue;
@@ -156,11 +216,13 @@ function creaTabellaCircuits(circuits) {
                 let _img = $("<img>");
                 _img.prop("src", circuits[i][index]);
                 _img.prop("height", "50");
-                row.append(_img);
+                cell.append(_img);
+                row.append(cell);
             }
         }
-        _table.append(row);
+        _tbody.append(row);
     }
+    _table.append(_tbody);
     $("#wrapper").append(_table);
 }
 
